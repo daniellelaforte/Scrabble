bbd3617445e898cac6549f4f5202d900cad0e056
Student <student@Mac-mini-31.local>
2015-10-04 14:11:27 -0600

    building out the board

--- scrabble.js
+++ scrabble.js
@@ -8,24 +8,50 @@ angular.module('myapp', ['dndLists'])
 angular.module('myapp').controller('SimpleDemoController', ['$scope', function($scope) {
 
   $scope.lists = [
+        // {
+        //     label: "Men",
+        //     allowedTypes: ['man'],
+        //     max: 4,
+        //     people: [
+        //         {name: "Bob", type: "man"},
+        //         {name: "Charlie", type: "man"},
+        //         {name: "Dave", type: "man"}
+        //     ]
+        // },
+        // {
+        //     label: "Women",
+        //     allowedTypes: ['woman'],
+        //     max: 4,
+        //     people: [
+        //         {name: "Alice", type: "woman"},
+        //         {name: "Eve", type: "woman"},
+        //         {name: "Peggy", type: "woman"}
+        //     ]
+        // },
         {
-            label: "Men",
-            allowedTypes: ['man'],
-            max: 4,
+            label: "People",
+            allowedTypes: ['man', 'woman'],
+            max: 1,
             people: [
-                {name: "Bob", type: "man"},
-                {name: "Charlie", type: "man"},
-                {name: "Dave", type: "man"}
+                {name: "Alice", type: "woman"},
+               
             ]
         },
         {
-            label: "Women",
-            allowedTypes: ['woman'],
-            max: 4,
+            label: "ScrabbleElem",
+            allowedTypes: ['man', 'woman'],
+            max: 1,
+            people: [
+               
+            ]
+        },
+        {
+            label: "People",
+            allowedTypes: ['man', 'woman'],
+            max: 1,
             people: [
                 {name: "Alice", type: "woman"},
-                {name: "Eve", type: "woman"},
-                {name: "Peggy", type: "woman"}
+               
             ]
         },
         {
