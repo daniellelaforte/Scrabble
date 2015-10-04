16147bfb3ddc0328e71f848814fd3a4ed22880bd
Student <student@Mac-mini-31.local>
2015-10-03 20:21:08 -0600

    changed to typed lists

--- scrabble.js
+++ scrabble.js
@@ -7,21 +7,45 @@ angular.module('myapp', ['dndLists'])
 
 angular.module('myapp').controller('SimpleDemoController', ['$scope', function($scope) {
 
-    $scope.models = {
-        selected: null,
-        lists: {"A": [], "B": []}
-    };
-
-    // Generate initial model
-    for (var i = 1; i <= 3; ++i) {
-        $scope.models.lists.A.push({label: "Item A" + i});
-        $scope.models.lists.B.push({label: "Item B" + i});
-    }
+  $scope.lists = [
+        {
+            label: "Men",
+            allowedTypes: ['man'],
+            max: 4,
+            people: [
+                {name: "Bob", type: "man"},
+                {name: "Charlie", type: "man"},
+                {name: "Dave", type: "man"}
+            ]
+        },
+        {
+            label: "Women",
+            allowedTypes: ['woman'],
+            max: 4,
+            people: [
+                {name: "Alice", type: "woman"},
+                {name: "Eve", type: "woman"},
+                {name: "Peggy", type: "woman"}
+            ]
+        },
+        {
+            label: "People",
+            allowedTypes: ['man', 'woman'],
+            max: 6,
+            people: [
+                {name: "Frank", type: "man"},
+                {name: "Mallory", type: "woman"},
+                {name: "Alex", type: "unknown"},
+                {name: "Oscar", type: "man"},
+                {name: "Wendy", type: "woman"}
+            ]
+        }
+    ];
 
     // Model to JSON for demo purpose
-    // $scope.$watch('models', function(model) {
-    //     $scope.modelAsJson = angular.toJson(model, true);
-    // }, true);
+    $scope.$watch('lists', function(lists) {
+        $scope.modelAsJson = angular.toJson(lists, true);
+    }, true);
 
 }]);
 
